b52695e0e7199c3f1213bebe8e8e0031656b4d54
Shamsiddinov Mladshiy <onlystudyacc0unt@gmail.com>
2024-04-23 15:27:44 +0500

    qwerty

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//1
+// let num1 = 6
+// let num2 = 4
+// alert(num1+num2)
+
+//2
+// let son = +12
+// alert(typeof son)
+
+//3
+// let r = Math.round(Math.random()*10)
+// alert(`siznig random soningiz ${r}`)
+// if(r % 2 === 0){
+//     console.log(`Bu random son juft son ${r}`)
+// }else if(r % 2 === 1){
+//     console.log(`Bu random son toq son ${r}`)
+// }
+
+//5
+// let arr = ['samaolyot','olma','shaftoli','Toshkent','daftar']
+// let arr2 = []
+
+// for() {
+//     console.log(arr2.push)
+// }
+
+
+// let ev = []
+
+// for(let)
